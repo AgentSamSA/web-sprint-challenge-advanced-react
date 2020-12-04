83e7daa4661025370b6d01aa6d13e673c83d9c5f
Samuel Lee <samuelxlee@gmail.com>
2020-12-04 09:04:20 -0800

    moved the local state into a custom hook, modifying the code to reflect this

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,14 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const useForm = (formFields) => {
+    const [values, setValues] = useState(formFields);
+
+    const handleChanges = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+      };
+
+      return [values, handleChanges];
+}
+
+export default useForm;
